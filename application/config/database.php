260d96845162c6f4889b508fc8c8c1b48649815b
Franky238 <frank238238@gmail.com>
2015-02-05 19:36:20 +0100

    zmena database.php pristupu do db

--- application/config/database.php
+++ application/config/database.php
@@ -5,7 +5,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'franky238';
+$db['default']['password'] = '';
 $db['default']['database'] = 'db_ionize';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
